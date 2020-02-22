33b79bb6e0b91f431542726248ce5425b4b062ae
rpuggian <rafael.puggian@mercadolivre.com>
2020-02-22 13:08:14 -0300

    updating auth0 credentials

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,7 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.eu-west-2.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-   domain: 'dev-6y08m7uy.auth0.com',            // Auth0 domain
-   clientId: 'DjFEYQBUio69EAD8PRMg15WJxczh2lMe',          // Auth0 client id
+   domain: 'dev-mp8zvicd.auth0.com',            // Auth0 domain
+   clientId: '29VL1Eo7qHOjOu7rd2s14tJwu3G1QxEy',          // Auth0 client id
    callbackUrl: 'http://localhost:3000/callback'
 }
